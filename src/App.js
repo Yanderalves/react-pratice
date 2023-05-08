ee8b90d3ec8a4a9977e2adf80b32467e62b56f15
Yander Alves de Carvalho <yander.carvalho@fitbank.com.br>
2023-05-08 15:03:54 -0300

    Praticando react - Jogo da velha

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
-function App() {
+export default function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const index = 0;
+  const onSquareClick = (i) => {
+    const nextSquares = [...squares]
+    nextSquares[i] = 'X';
+    setSquares(nextSquares);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='board-row'>
+        <Square onSquareClick={() => onSquareClick(0)} value={squares[0]} />
+        <Square onSquareClick={() => onSquareClick(1)} value={squares[1]} />
+        <Square onSquareClick={() => onSquareClick(2)} value={squares[2]} />
+      </div>
+      <div className='board-row'>
+        <Square onSquareClick={() => onSquareClick(3)} value={squares[3]} />
+        <Square onSquareClick={() => onSquareClick(4)} value={squares[4]} />
+        <Square onSquareClick={() => onSquareClick(5)} value={squares[5]} />
+      </div>
+      <div className='board-row'>
+        <Square onSquareClick={() => onSquareClick(6)} value={squares[6]} />
+        <Square onSquareClick={() => onSquareClick(7)} value={squares[7]} />
+        <Square onSquareClick={() => onSquareClick(8)} value={squares[8]} />
+      </div>
+    </>
+  )
 }
 
-export default App;
+function Square({ value, onSquareClick }) {
+
+  return (
+    <button onClick={onSquareClick} className="square">{value}</button>
+  )
+}
